371fc536d117aceb84c96e4db2f00ff2fdbbc35b
LACOSTAR91 <66567863+LACOSTAR91@users.noreply.github.com>
2021-08-26 21:02:16 +0200

    Delete tests directory

--- tests/index.js
+++
@@ -1,8 +0,0 @@
-const NightAPI = require('night-api');
-const api = new NightAPI("ZzkQJW9mfX-2iCp6WGXvNgWzhRPQitv-wJzIrRpg19");
-
-async function blague() {
-    const blague = await api.jokes.random({ disallow: ['global', 'limit', 'beauf', 'dark', 'blondes'] });
-    console.log(blague)
-};
-blague();
