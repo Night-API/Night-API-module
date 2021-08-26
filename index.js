bf66e668eeef8398bb96dee5772521ab382f2372
LACOSTAR91 <66567863+LACOSTAR91@users.noreply.github.com>
2021-08-26 21:03:08 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const Nightapi = require("./src/NightAPI");
-function NightAPI(token) { return new Nightapi(token); };
-NightAPI.API = Nightapi; 
-/**
- * @returns {string}
-*/
-NightAPI.version = require("./package.json").version;
-module.exports = NightAPI;
